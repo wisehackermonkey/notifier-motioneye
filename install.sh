921a5cac32bea74bda746f60b141a46194893038
wisehackermonkey <oranbusiness@gmail.com>
2021-10-19 19:04:50 -0700

    [add] install checks for install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-read ~/.bashrc
-if dietpi-software --help ;then
-    dietpi-software install 70
-    # 
-else
+bash ~/.bashrc
+if ! gpio -v > /dev/null;then
+    echo "installing wiringpi"
     sudo apt-get install wiringpi
+    else
+        echo "wiringpi        [Installed ✅]"
+
 fi
-npm install -g forever 
-npm install -g forever-service
+
+if ! forever-service -V > /dev/null;then
+    echo "installing forever-service"
+
+    npm install -g forever 
+    npm install -g forever-service
+    else
+    echo "forever-service [Installed ✅]"
+fi

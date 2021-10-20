83b787af36e8dd992f8ca761c901514a294bf53d
wisehackermonkey <oranbusiness@gmail.com>
2021-10-19 18:58:18 -0700

    [add]

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 read ~/.bashrc
 if dietpi-software --help ;then
-dietpi-software install 70
+    dietpi-software install 70
+    # 
+else
     sudo apt-get install wiringpi
-
 fi
 npm install -g forever 
 npm install -g forever-service

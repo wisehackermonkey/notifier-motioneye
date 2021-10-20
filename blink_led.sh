99b0367a8672172b63b850d7298c8a0a76cc4f85
wisehackermonkey <oranbusiness@gmail.com>
2021-10-19 17:56:13 -0700

    [add] test blink led script

---
+++ blink_led.sh
@@ -0,0 +1,29 @@
+# which gpio pin is used to blick the led
+# use `gpio readall` to get your pin out use `wPi` pin number here
+PIN=15
+if gpio -v > /tmp/log.log; then
+
+    # set output pin to output
+    gpio mode $PIN out
+    gpio write $PIN 0
+# blink a few times to indicate that led is working 
+    # and that the script has started
+    gpio write $PIN 1
+    sleep .1
+    gpio write $PIN 0
+    sleep .1
+    gpio write $PIN 1
+    sleep .1
+    gpio write $PIN 0
+    sleep .1
+
+    echo "Blink test started"
+    while true; do
+        echo "Blink"
+        gpio write $PIN 1
+        sleep 1
+        gpio write $PIN 0
+        sleep 1
+    done
+
+fi

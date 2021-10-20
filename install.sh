e8549619ed1ce4e415322e5bae5e05dcfefd61f0
wisehackermonkey <oranbusiness@gmail.com>
2021-10-19 19:09:55 -0700

    [add] installl

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bash ~/.bashrc
-if ! gpio -v > /dev/null;then
+if ! gpio -v 2>&1 /dev/null;then
     echo "installing wiringpi"
     sudo apt-get install wiringpi
     else
@@ -8,7 +8,7 @@ if ! gpio -v > /dev/null;then
 
 fi
 
-if ! forever-service -V > /dev/null;then
+if ! forever-service -V 2>&1 /dev/null;then
     echo "installing forever-service"
 
     npm install -g forever 
